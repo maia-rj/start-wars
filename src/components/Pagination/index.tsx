3f8369db174e0ff9fd7527ea6a65379e8a71c811
Maia <102555622+maia-rj@users.noreply.github.com>
2024-05-17 11:23:09 -0300

    feat: create pagination component

---
+++ src/components/Pagination/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Icon from '@mdi/react';
+import { PaginationButton, PaginationContainer, PaginationInfo } from './styles';
+import { mdiChevronLeft, mdiChevronRight } from '@mdi/js';
+
+type PaginationProps = {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+};
+
+
+const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages, onPageChange }) => {
+  const handlePrevious = () => {
+    if (currentPage > 1) {
+      onPageChange(currentPage - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (currentPage < totalPages) {
+      onPageChange(currentPage + 1);
+    }
+  };
+
+  return (
+    <PaginationContainer>
+      <PaginationButton onClick={handlePrevious} disabled={currentPage === 1}>
+        <Icon path={mdiChevronLeft} size={1} />
+      </PaginationButton>
+      <PaginationInfo>{currentPage} / {totalPages}</PaginationInfo>
+      <PaginationButton onClick={handleNext} disabled={currentPage === totalPages}>
+        <Icon path={mdiChevronRight} size={1} />
+      </PaginationButton>
+    </PaginationContainer>
+  );
+};
+
+export default Pagination;
---
+++ src/components/Pagination/styles.ts
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const PaginationContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 20px 0;
+`;
+
+export const PaginationButton = styled.button`
+  background-color: #6200ea;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  padding: 10px 20px;
+  margin: 0 10px;
+  cursor: pointer;
+  font-size: 16px;
+  transition: background-color 0.3s;
+
+  &:disabled {
+    background-color: #bdbdbd;
+    cursor: not-allowed;
+  }
+
+  &:not(:disabled):hover {
+    background-color: #3700b3;
+  }
+`;
+
+export const PaginationInfo = styled.span`
+  font-size: 16px;
+  color: #333;
+`;
