adf61d0c2619c00d2ddada78265bf6b62208d0c3
Maia <102555622+maia-rj@users.noreply.github.com>
2024-05-17 11:25:59 -0300

    feat: wip app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-
-import React from 'react';
-import GlobalStyle from './GlobalStyle';
-import CharacterList from './components/CharacterList';
+import React, { useState } from "react";
+import GlobalStyle from "./GlobalStyle";
+import CharacterList from "./components/CharacterList";
+import Pagination from "./components/Pagination";
+import { useFetchCharacters } from "./hooks/useFetchCharacters";
 
 const App: React.FC = () => {
-
+  const [currentPage, setCurrentPage] = useState(1);
+  const { loading, error, totalPages } = useFetchCharacters(currentPage);
   return (
     <div>
-      <GlobalStyle/>
+      <GlobalStyle />
       <h1>Star Wars Characters</h1>
-      <CharacterList />
+
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {!loading && !error && (
+        <>
+          <CharacterList />
+          <Pagination
+            currentPage={currentPage}
+            totalPages={totalPages}
+            onPageChange={setCurrentPage}
+          />
+        </>
+      )}
     </div>
   );
 };
